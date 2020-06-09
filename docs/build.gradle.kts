d4db4b28d85fe94cf391f66a6d1d5f1cfe22a47c
Radimir Sorokin <radimir.sorokin@jetbrains.com>
2020-06-10 00:57:18 +0200

    add site publication

--- docs/build.gradle.kts
+++ docs/build.gradle.kts
@@ -1,42 +1,32 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-// Buildscript dependencies and plugins
-//----------------------------------------------------------------------------------------------------------------------
-
 plugins {
-    // Check latest version at https://plugins.gradle.org/plugin/com.eden.orchidPlugin
     id("com.eden.orchidPlugin") version "0.21.0"
-    kotlin("jvm") version "1.3.72"
 }
 
-// Orchid setup
-//----------------------------------------------------------------------------------------------------------------------
 repositories {
     jcenter()
+//    maven(url = "https://kotlin.bintray.com/kotlinx/")
 }
 
 dependencies {
     val orchid_version = "0.21.0"
-    compile("io.github.javaeden.orchid:OrchidCore:$orchid_version")
-    orchidCompile("io.github.javaeden.orchid:OrchidCore:$orchid_version")
-    orchidRuntime("io.github.javaeden.orchid:OrchidPosts:$orchid_version")
-    orchidRuntime("io.github.javaeden.orchid:OrchidPages:$orchid_version")
-    orchidRuntime("io.github.javaeden.orchid:OrchidWiki:$orchid_version")
-    orchidRuntime("io.github.javaeden.orchid:OrchidSearch:$orchid_version")
-    orchidRuntime("io.github.javaeden.orchid:OrchidTaxonomies:$orchid_version")
-    orchidRuntime("io.github.javaeden.orchid:OrchidWritersBlocks:$orchid_version")
-    orchidRuntime("io.github.javaeden.orchid:OrchidSyntaxHighlighter:$orchid_version")
-    orchidRuntime("io.github.javaeden.orchid:OrchidEditorial:$orchid_version")
-    orchidRuntime("io.github.javaeden.orchid:OrchidDocs:$orchid_version")
-    orchidRuntime("io.github.javaeden.orchid:OrchidJavadoc:$orchid_version")
-    orchidRuntime("io.github.javaeden.orchid:OrchidPluginDocs:$orchid_version")
-    orchidRuntime("io.github.javaeden.orchid:OrchidGithub:$orchid_version")
+    orchidRuntimeOnly("io.github.javaeden.orchid:OrchidCore:$orchid_version")
+    orchidRuntimeOnly("io.github.javaeden.orchid:OrchidPosts:$orchid_version")
+    orchidRuntimeOnly("io.github.javaeden.orchid:OrchidPages:$orchid_version")
+    orchidRuntimeOnly("io.github.javaeden.orchid:OrchidWiki:$orchid_version")
+    orchidRuntimeOnly("io.github.javaeden.orchid:OrchidSearch:$orchid_version")
+    orchidRuntimeOnly("io.github.javaeden.orchid:OrchidTaxonomies:$orchid_version")
+    orchidRuntimeOnly("io.github.javaeden.orchid:OrchidWritersBlocks:$orchid_version")
+    orchidRuntimeOnly("io.github.javaeden.orchid:OrchidSyntaxHighlighter:$orchid_version")
+    orchidRuntimeOnly("io.github.javaeden.orchid:OrchidEditorial:$orchid_version")
+    orchidRuntimeOnly("io.github.javaeden.orchid:OrchidDocs:$orchid_version")
+    orchidRuntimeOnly("io.github.javaeden.orchid:OrchidJavadoc:$orchid_version")
+    orchidRuntimeOnly("io.github.javaeden.orchid:OrchidPluginDocs:$orchid_version")
+    orchidRuntimeOnly("io.github.javaeden.orchid:OrchidGithub:$orchid_version")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
+version = "1.0"
 
 orchid {
     args = listOf("--experimentalSourceDoc")
+    githubToken = rootProject.property("githubToken") as String
 }
