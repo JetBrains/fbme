f26de0ffc73c854faf264efd491bb38cc84e555d
Radimir Sorokin <radimir.sorokin@jetbrains.com>
2020-11-20 17:58:23 +0100

    Minor refactoring gradle scirpt, buildBootstrap tracks changes in buildsolutions, clean task cleans files generated by buildBootstrap

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 import de.undercouch.gradle.tasks.download.Download
-import org.fbme.gradle.*
+import org.fbme.gradle.MpsExtension
 
 val mpsMajor = "2020.2"
 val mpsMinor = "2"
@@ -35,36 +35,19 @@ dependencies {
     ant_lib("org.apache.ant:ant-junit:1.10.1")
 }
 
-subprojects {
-    pluginManager.withPlugin("org.fbme.gradle.mps") {
-        if (the<MpsExtension>().hasBuildSolution) {
-            dependencies {
-                "mpsBinaries"("com.jetbrains:MPS:$mpsMajor.$mpsMinor@zip")
-                "antBinaries"("org.apache.ant:ant-junit:1.10.1")
-            }
-            tasks.named("mpsPrepare") {
-                dependsOn(rootProject.tasks.named("buildBootstrap"))
-            }
-        }
-        rootProject.tasks.named("build") {
-            dependsOn(tasks.named("build"))
-        }
-    }
-}
-
-tasks.register<Download>("downloadJBRZip") {
+val downloadJbrZip by tasks.registering(Download::class) {
     src("https://jetbrains.bintray.com/intellij-jbr/jbrsdk-11_0_6-osx-x64-b520.43.tar.gz")
     dest(file("lib/jbr.tar.gz"))
     overwrite(false)
 }
 
-tasks.register<Copy>("downloadJBR") {
-    dependsOn("downloadJBRZip")
+val downloadJbr by tasks.registering(Copy::class) {
+    dependsOn(downloadJbrZip)
     from(tarTree("lib/jbr.tar.gz"))
     into(file("lib/jbr"))
 }
 
-tasks.register<Copy>("downloadDependencies") {
+val downloadDependencies by tasks.registering(Copy::class) {
     dependsOn(mps)
 
     mps.files.forEach {
@@ -73,41 +56,57 @@ tasks.register<Copy>("downloadDependencies") {
     into("lib")
 }
 
-fun Task.antexec(path: String, task: String? = null) {
+val buildBootstrap by tasks.registering {
+    inputs.dir("buildscripts/models")
+    inputs.file("build-bootstrap.xml")
+    outputs.dir("buildscripts/source_gen")
+
+
+    dependsOn(downloadDependencies)
+
+    antexec("build-bootstrap.xml", "generate")
+}
+
+
+val build by tasks.getting {
+    dependsOn(buildBootstrap)
+}
+
+subprojects {
+    pluginManager.withPlugin("org.fbme.gradle.mps") {
+        if (the<MpsExtension>().hasBuildSolution) {
+            buildBootstrap.get().inputs.dir("$projectDir/buildsolution/models")
+            dependencies {
+                "mpsBinaries"("com.jetbrains:MPS:$mpsMajor.$mpsMinor@zip")
+                "antBinaries"("org.apache.ant:ant-junit:1.10.1")
+            }
+            tasks.named("mpsPrepare") {
+                dependsOn(buildBootstrap)
+            }
+        }
+        build.dependsOn(tasks.named("build"))
+    }
+}
+
+fun Task.antexec(path: String, vararg tasks: String) {
     doLast {
         javaexec {
             main = "org.apache.tools.ant.launch.Launcher"
             classpath = ant_lib
 
             args("-Dbasedir=$projectDir", "-buildfile", file(path))
-            if (task != null) {
-                args(task)
-            }
+            args(*tasks)
         }
     }
 }
 
-tasks.register("buildBootstrap") {
-    inputs.dir("buildscripts/models")
-    inputs.file("build-bootstrap.xml")
-    outputs.dir("buildscripts/source_gen")
-
-    dependsOn("downloadDependencies")
-
-    antexec("build-bootstrap.xml", "generate")
-}
-
-tasks.named("build") {
-    dependsOn("buildBootstrap")
-}
-
-tasks.register("\$buildRcp") {
-    dependsOn("build")
+val buildRcp0 by tasks.registering {
+    dependsOn(build)
     antexec("build/build-rcp.xml")
 }
 
-tasks.register<Copy>("copyStartupScripts") {
-    dependsOn("buildBootstrap")
+val copyStartupScripts by tasks.registering(Copy::class) {
+    dependsOn(buildBootstrap)
 
     from("buildscripts/source_gen/org/fbme/ide/build")
 
@@ -119,18 +118,19 @@ tasks.register<Copy>("copyStartupScripts") {
     into("build/startup")
 }
 
-tasks.register("buildRcp") {
-    dependsOn("\$buildRcp", "copyStartupScripts")
+val buildRcp by tasks.registering {
+    dependsOn(buildRcp0, copyStartupScripts)
     antexec("build/build-rcpdistrib.xml")
 }
 
-tasks.register("buildRcpWithJBR") {
-    dependsOn("\$buildRcp", "copyStartupScripts", "downloadJBR")
+val buildRcpWithJbr by tasks.registering {
+    dependsOn(buildRcp0, copyStartupScripts, downloadJbr)
     antexec("build/build-rcpdistrib-jbr.xml")
 }
 
-tasks.register<Copy>("macosBinaries") {
-    dependsOn("buildRcpWithJBR")
+val macosBinaries by tasks.registering(Copy::class) {
+    dependsOn(buildRcpWithJbr)
+
     from(zipTree(file("build/artifacts/fbme_rcp_distrib_jbr/fbme-202.SNAPSHOT.macos.zip"))) {
         eachFile {
             relativePath = RelativePath(true, *relativePath.segments.drop(1).toTypedArray())
@@ -150,6 +150,7 @@ tasks.register<Copy>("macosBinaries") {
     }
 }
 
-tasks.named<Delete>("clean") {
+val clean by tasks.getting {
     delete("lib")
+    antexec("build-bootstrap.xml", "clean", "cleanSources")
 }
