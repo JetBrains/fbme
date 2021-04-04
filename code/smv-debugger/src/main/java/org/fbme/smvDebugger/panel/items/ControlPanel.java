c10d2810dd27bee577542237555096d25d3e067a
Radimir Sorokin <radimir.sorokin@jetbrains.com>
2021-04-04 22:59:29 +0300

    Cosmetic changes for smm-debugger ui

--- code/smv-debugger/src/main/java/org/fbme/smvDebugger/panel/items/ControlPanel.java
+++ code/smv-debugger/src/main/java/org/fbme/smvDebugger/panel/items/ControlPanel.java
@@ -1,76 +1,76 @@
 package org.fbme.smvDebugger.panel.items;
 
-import javax.swing.JPanel;
+import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBPanel;
+import org.fbme.smvDebugger.model.Counterexample;
 import org.fbme.smvDebugger.panel.mvc.DebugPanelMVCItem;
 import org.fbme.smvDebugger.panel.mvc.DebugPanelModel;
-import org.fbme.smvDebugger.model.Counterexample;
 import org.fbme.smvDebugger.visualization.SystemHighlighter;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import javax.swing.JLabel;
 
-public class ControlPanel extends JPanel implements DebugPanelMVCItem {
-  private static final String STEP_LABEL_NAME = "Current step:";
-  private static final String TIME_LABEL_NAME = "Current time:";
-  private static final String CONDITION_LABEL_NAME = "Condition:";
+import java.awt.*;
 
-  private final StepChanger stepChanger;
-  private final GlobalTime globalTime;
-  private final ConditionNavigator conditionNavigator;
+public class ControlPanel extends JBPanel implements DebugPanelMVCItem {
+    private static final String STEP_LABEL_NAME = "Current step:";
+    private static final String TIME_LABEL_NAME = "Current time:";
+    private static final String CONDITION_LABEL_NAME = "Condition:";
 
-  public ControlPanel(final Counterexample counterexample, final SystemHighlighter systemHighlighter) {
-    stepChanger = new StepChanger(counterexample, systemHighlighter);
-    globalTime = new GlobalTime();
-    conditionNavigator = new ConditionNavigator(counterexample);
-  }
+    private final StepChanger stepChanger;
+    private final GlobalTime globalTime;
+    private final ConditionNavigator conditionNavigator;
 
-  @Override
-  public void setPanelModel(final DebugPanelModel model) {
-    stepChanger.setPanelModel(model);
-    globalTime.setPanelModel(model);
-    conditionNavigator.setPanelModel(model);
-  }
+    public ControlPanel(final Counterexample counterexample, final SystemHighlighter systemHighlighter) {
+        stepChanger = new StepChanger(counterexample, systemHighlighter);
+        globalTime = new GlobalTime();
+        conditionNavigator = new ConditionNavigator(counterexample);
+    }
 
-  @Override
-  public void initView() {
-    stepChanger.initView();
-    globalTime.initView();
-    conditionNavigator.initView();
+    @Override
+    public void setPanelModel(final DebugPanelModel model) {
+        stepChanger.setPanelModel(model);
+        globalTime.setPanelModel(model);
+        conditionNavigator.setPanelModel(model);
+    }
 
-    setLayout(new GridBagLayout());
-    final GridBagConstraints constraints = new GridBagConstraints();
-    constraints.fill = GridBagConstraints.HORIZONTAL;
-    constraints.weightx = 1;
+    @Override
+    public void initView() {
+        stepChanger.initView();
+        globalTime.initView();
+        conditionNavigator.initView();
 
-    constraints.gridx = 0;
-    constraints.gridy = 0;
-    add(new JLabel(STEP_LABEL_NAME), constraints);
+        setLayout(new GridBagLayout());
+        final GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 1;
 
-    constraints.gridx = 1;
-    constraints.gridy = 0;
-    add(stepChanger, constraints);
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        add(new JBLabel(STEP_LABEL_NAME), constraints);
 
-//    constraints.gridx = 0;
-//    constraints.gridy = 1;
-//    add(new JLabel(TIME_LABEL_NAME), constraints);
-//
-//    constraints.gridx = 1;
-//    constraints.gridy = 1;
-//    add(globalTime, constraints);
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        add(stepChanger, constraints);
 
-    constraints.gridx = 0;
-    constraints.gridy = 2;
-    add(new JLabel(CONDITION_LABEL_NAME), constraints);
+    //    constraints.gridx = 0;
+    //    constraints.gridy = 1;
+    //    add(new JBLabel(TIME_LABEL_NAME), constraints);
+    //
+    //    constraints.gridx = 1;
+    //    constraints.gridy = 1;
+    //    add(globalTime, constraints);
 
-    constraints.gridx = 1;
-    constraints.gridy = 2;
-    add(conditionNavigator, constraints);
-  }
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        add(new JBLabel(CONDITION_LABEL_NAME), constraints);
 
-  @Override
-  public void initController() {
-    stepChanger.initController();
-    globalTime.initController();
-    conditionNavigator.initController();
-  }
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        add(conditionNavigator, constraints);
+    }
+
+    @Override
+    public void initController() {
+        stepChanger.initController();
+        globalTime.initController();
+        conditionNavigator.initController();
+    }
 }
