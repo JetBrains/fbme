b3ec7131d045bcab3d3fe18517ac744b677badbb
Valentina Danilova <valentinka-danilova@mail.ru>
2022-08-06 05:25:24 +0300

    .

---
+++ code/library/src/main/kotlin/org/fbme/lib/iec61499/declarations/DependentFilesDefinition.kt
@@ -0,0 +1,5 @@
+package org.fbme.lib.iec61499.declarations
+
+interface DependentFilesDefinition {
+    var value: String
+}
---
+++ code/library/src/main/kotlin/org/fbme/lib/iec61499/declarations/HMIInterfaceTypeDeclaration.kt
@@ -0,0 +1,10 @@
+package org.fbme.lib.iec61499.declarations
+
+import org.fbme.lib.common.ContainedElement
+import org.fbme.lib.common.Declaration
+import org.fbme.lib.common.Reference
+
+interface HMIInterfaceTypeDeclaration : Declaration, ContainedElement {
+//    val symbol: SymbolDefinition
+//    val blockDeclaration: Reference<ServiceInterfaceFBTypeDeclaration>
+}
