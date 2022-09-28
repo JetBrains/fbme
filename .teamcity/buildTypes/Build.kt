0fea5783f6fa463f8be514dc0a7e23ae16ede54c
radimir.sorokin <radimir.sorokin@jetbrains.com>
2022-09-28 22:34:05 +0200

    Teamcity config: Build configuration keeps artifacts directory

--- .teamcity/buildTypes/Build.kt
+++ .teamcity/buildTypes/Build.kt
@@ -5,7 +5,7 @@ object Build : FbmeBuildType(
         useSharedBuildNumber()
         useMpsPlatform()
 
-        artifactRules = "build/artifacts/fbme_rcp_shared"
+        artifactRules = "+:build/artifacts/ => build/artifacts/"
 
         gradleStep("clean assembleRcpShared -x test")
     }
