82754ca78085965b155001c2817de2466e374a4c
”Dmitri <dmitri.zd1958@gmail.com>
2023-06-19 14:24:30 +0200

    Delete unused exception

--- code/richediting/src/main/kotlin/org/fbme/ide/richediting/utils/exceptions/FBMEException.kt
+++
@@ -1,9 +0,0 @@
-package org.fbme.ide.richediting.utils.exceptions
-
-class FBMEException : RuntimeException {
-    constructor() : super()
-
-    constructor(message: String) : super(message)
-
-    constructor(message: String, cause: Throwable) : super(message, cause)
-}
