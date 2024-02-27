d9ba3ec59a3e3c51cb32b1b1b60bda2ca8dcad34
Rus <aliev.345@yandex.ru>
2024-02-27 18:53:36 +0300

    Add unit tests for `BasicFBTypeLuaTranslator`

--- code/4diac-integration/build.gradle.kts
+++ code/4diac-integration/build.gradle.kts
@@ -6,12 +6,28 @@ plugins {
     kotlin
 }
 
+sourceSets {
+    test {
+        java.srcDir("src/test/kotlin")
+        resources {
+            srcDir("src/test/resources")
+        }
+    }
+}
 dependencies {
     mpsImplementation(project(":code:library", "mps"))
     mpsImplementation(project(":code:language", "mps"))
     mpsImplementation(project(":code:platform", "mps"))
 
     compileOnly(project(":code:library"))
+
+    testImplementation("io.mockk:mockk:1.13.9") {
+        exclude(group="org.jetbrains.kotlin")
+    }
+    testRuntimeOnly("org.jetbrains.intellij.deps:jdom:2.0.6")
+    testImplementation(kotlin("reflect"))
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.4.2")
+    testImplementation(project(":code:library"))
 }
 
 mps {
@@ -21,4 +37,8 @@ mps {
 
 val compileKotlin by tasks.getting(KotlinCompile::class) {
     kotlinOptions.freeCompilerArgs = listOf("-Xjvm-default=all")
+}
+
+val test by tasks.getting(Test::class) {
+    useJUnitPlatform()
 }
