3b8a7b9e9891728567e00ed028b8ac77b5c2ba20
Rus <aliev.345@yandex.ru>
2024-03-13 18:28:48 +0300

    Add device communication handler for deployment.

---
+++ code/4diac-integration/src/integration-test/kotlin/org/fbme/ide/integration/fordiac/deploy/EthernetDeviceCommunicationHandlerTest.kt
@@ -0,0 +1,57 @@
+package org.fbme.ide.integration.fordiac.deploy
+
+import org.junit.After
+import org.junit.Test
+import java.io.InputStream
+import java.io.OutputStream
+import java.net.ServerSocket
+
+class EthernetDeviceCommunicationHandlerTest {
+    private val handler = EthernetDeviceCommunicationHandler()
+    private val port = 9999
+
+    private fun serverThread(code: (writer: OutputStream, reader: InputStream) -> Unit) = Thread {
+        val server = ServerSocket(port)
+        val client = server.accept()
+
+        code(client.outputStream, client.inputStream)
+
+        client.close()
+        server.close()
+    }
+
+    @After
+    fun disconnectHandler() {
+        handler.disconnect()
+    }
+
+    @Test
+    fun `communication with server`() {
+        val request = "hello"
+        val response = "ok"
+
+        val thread = serverThread { writer, reader ->
+            val additionalBytesAmount = 6
+            val message = reader.readNBytes(request.length + additionalBytesAmount)
+
+            val iecTag: Byte = 80
+            val prefix = byteArrayOf(iecTag, 0, 0, iecTag, 0, request.length.toByte())
+
+            println("\tbefore assert")
+            assert((prefix + request.toByteArray()).contentEquals(message))
+
+            writer.write(byteArrayOf(0, 0, response.length.toByte()) + response.toByteArray())
+            reader.close()
+            writer.close()
+        }
+
+        thread.start()
+
+        handler.connect("localhost", port)
+        val actualResponse = handler.send(destination = "", request = request)
+
+        assert(response.contentEquals(actualResponse))
+
+        thread.join()
+    }
+}
