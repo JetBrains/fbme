47f19563de024a7fc8a5a0be599eaab752a2072b
Daniil Akifev <daniil.akifev@jetbrains.com>
2024-06-05 01:29:20 +0300

    Build a partial trace when building a full trace ends with error

--- code/debugger/src/main/kotlin/org/fbme/debugger/RuntimeTraceSynchronizer.kt
+++ code/debugger/src/main/kotlin/org/fbme/debugger/RuntimeTraceSynchronizer.kt
@@ -3,13 +3,14 @@ package org.fbme.debugger
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import jetbrains.mps.project.MPSProject
+import org.fbme.debugger.common.change.ComplexChange
 import org.fbme.debugger.common.resolveFB
 import org.fbme.debugger.common.resolvePath
-import org.fbme.debugger.common.state.ResourceState
+import org.fbme.debugger.common.state.*
 import org.fbme.debugger.common.trace.ExecutionTrace
 import org.fbme.debugger.common.trace.TraceItem
 import org.fbme.debugger.common.typeOfParameter
-import org.fbme.debugger.common.value.Value
+import org.fbme.debugger.common.value.*
 import org.fbme.debugger.common.valueOfParameter
 import org.fbme.debugger.simulator.FBSimulator
 import org.fbme.debugger.simulator.ResourceSimulator
@@ -83,6 +84,67 @@ class RuntimeTraceSynchronizer(
 
         val initialState = trace.items.last().state as ResourceState
 
+        val uniqueResolvedWatches = readWatchesRequests.toSet().map { watches ->
+            watches.mapKeys { it.key.resolve(repository) }
+        }
+
+        val traceItems = try {
+            fullTrace(initialState, uniqueResolvedWatches)
+        } catch (e: Throwable) {
+            partialTrace(initialState, uniqueResolvedWatches)
+        }
+
+        trace.addAll(traceItems)
+    }
+
+    private fun partialTrace(
+        initialState: ResourceState,
+        uniqueResolvedWatches: List<Map<Watchable, String>>
+    ): List<TraceItem> {
+        return buildList {
+            var currentState = initialState
+
+            for (watches in uniqueResolvedWatches) {
+                val changes = getChanges(currentState, watches)
+
+                if (changes.isEmpty()) continue
+
+                val nextState = ResourceState(currentState) {
+                    for ((path, newValue) in changes) {
+                        val fbState = resolveFB(path.dropLast(1))
+                        val name = path.last()
+
+                        fbState.apply {
+                            when (name) {
+                                in inputEvents -> inputEvents[name] = newValue.toInt()
+                                in outputEvents -> outputEvents[name] = newValue.toInt()
+                                in inputVariables -> inputVariables[name] = inputVariables[name]!!.copyWith(newValue)
+                                in outputVariables -> outputVariables[name] = outputVariables[name]!!.copyWith(newValue)
+                            }
+                            if (this is BasicFBState) {
+                                if (name in internalVariables) {
+                                    internalVariables[name] = internalVariables[name]!!.copyWith(newValue)
+                                } else if (name == "\$ECC") {
+                                    activeState = newValue
+                                }
+                            }
+                        }
+                    }
+                }
+
+                val traceItem = TraceItem(nextState, emptyList(), ComplexChange(emptyList()), true)
+
+                add(traceItem)
+
+                currentState = nextState
+            }
+        }
+    }
+
+    private fun fullTrace(
+        initialState: ResourceState,
+        uniqueResolvedWatches: List<Map<Watchable, String>>
+    ): List<TraceItem> {
         val startNode = TraceNode(
             traceSegment = listOf(trace.items.last()),
             simulator = ResourceSimulator(resourceDeclaration, initialState)
@@ -92,10 +154,9 @@ class RuntimeTraceSynchronizer(
 
         var candidates = listOf(startNode)
 
-        requestsLoop@ for ((watchesIndex, watches) in readWatchesRequests.withIndex()) {
-            val newWatches = watches.mapKeys { it.key.resolve(repository) }
+        requestsLoop@ for (watches in uniqueResolvedWatches) {
 
-            val changes = getChanges(prevState, newWatches)
+            val changes = getChanges(prevState, watches)
             if (changes.isEmpty()) {
                 continue@requestsLoop
             }
@@ -104,7 +165,7 @@ class RuntimeTraceSynchronizer(
                 val fbType = resourceDeclaration.resolvePath(path.dropLast(1))
                 val portName = path.last()
                 fbType is ServiceInterfaceFBTypeDeclaration && portName in fbType.outputEvents.map { it.name }
-                    || fbType.name == "E_CYCLE" && portName == "EO" // TODO: handle these cases more generic way
+                        || fbType.name == "E_CYCLE" && portName == "EO" // TODO: handle these cases more generic way
             }
 
             if (serviceOutputEventChanges.isNotEmpty()) {
@@ -134,7 +195,7 @@ class RuntimeTraceSynchronizer(
             candidatesLoop@ for (candidate in candidates) {
                 val traceSegment = candidate.traceSegment
                 tailSearchLoop@ for ((itemIndex, traceItem) in traceSegment.withIndex()) {
-                    val itemChanges = getChanges(traceItem.state as ResourceState, newWatches)
+                    val itemChanges = getChanges(traceItem.state as ResourceState, watches)
                     if (itemChanges.isEmpty()) {
                         prevState = traceItem.state
                         newCandidates += TraceNode(
@@ -153,13 +214,13 @@ class RuntimeTraceSynchronizer(
             candidates = newCandidates
         }
 
-        var cur: TraceNode? = candidates.first()
-        val result = mutableListOf<TraceItem>()
-        while (cur != null) {
-            result += cur.traceSegment.reversed()
-            cur = cur.parent
-        }
-        trace.addAll(result.reversed().drop(1))
+        return buildList {
+            var cur: TraceNode? = candidates.first()
+            while (cur != null) {
+                addAll(cur.traceSegment.reversed())
+                cur = cur.parent
+            }
+        }.reversed().drop(1)
     }
 
     private fun initPorts() {
@@ -172,6 +233,16 @@ class RuntimeTraceSynchronizer(
                 fbState.inputVariables[portName] = value
             }
         }
+    }
+
+    private fun <T> Value<T>.copyWith(newValue: String): Value<T> {
+        @Suppress("UNCHECKED_CAST")
+        return when (this) {
+            is BooleanValue -> BooleanValue(newValue.toBoolean())
+            is IntValue -> IntValue(newValue.toInt())
+            is StringValue -> StringValue(newValue)
+            is TimeValue -> TimeValue(newValue)
+        } as Value<T>
     }
 
     companion object {
