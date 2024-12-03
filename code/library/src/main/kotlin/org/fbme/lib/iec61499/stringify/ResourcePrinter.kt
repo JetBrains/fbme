d01bc362768f0fbae47828671c3e3dbea0c9893c
Esa Paavilainen <esa.paavilainen@aalto.fi>
2024-12-03 15:12:08 +0200

    ResourcePrinter prints the namespace now.

--- code/library/src/main/kotlin/org/fbme/lib/iec61499/stringify/ResourcePrinter.kt
+++ code/library/src/main/kotlin/org/fbme/lib/iec61499/stringify/ResourcePrinter.kt
@@ -1,12 +1,20 @@
 package org.fbme.lib.iec61499.stringify
 
+import org.fbme.lib.iec61499.NamespaceFinder
 import org.fbme.lib.iec61499.declarations.ResourceDeclaration
 import org.jdom.Element
 
 class ResourcePrinter(declaration: ResourceDeclaration) :
     DeclarationPrinterBase<ResourceDeclaration>(declaration, "Resource") {
     override fun printDeclarationBody(element: Element) {
-        element.setAttribute("Type", this.element.typeReference.presentation)
+        val type = this.element.typeReference.presentation
+        element.setAttribute("Type", type)
+        val namespace = if (this.element.namespace != null) {
+            this.element.namespace
+        } else {
+            NamespaceFinder.getNamespace(type)
+        }
+        element.setAttribute("Namespace", namespace)
         ParameterAssignmentPrinter.printAll(this.element.parameters, element)
         addNullableContent(element, FBNetworkPrinter(this.element.network).printNullable())
     }
