2bdcebd85768e0f596d366298c4a56aab40e3a88
Esa Paavilainen <esa.paavilainen@aalto.fi>
2024-12-04 18:57:01 +0200

    Expanded SystemEcoConverter.
    There's a major flaw when it comes to modifying the system file in FBME,
    which causes applications to be inconsistent from resources. THerefore,
    I introduced a new function to the EcoConverter, fixInconsistencies. It
    should now fix the mismathcing constant inputs in the interface of any
    FB in any application (compared to resource).

--- code/nxt-integration/src/main/kotlin/org/fbme/integration/nxt/exporter/SystemDeclarationEcoConverter.kt
+++ code/nxt-integration/src/main/kotlin/org/fbme/integration/nxt/exporter/SystemDeclarationEcoConverter.kt
@@ -1,5 +1,7 @@
 package org.fbme.integration.nxt.exporter
 
+import com.intellij.psi.impl.source.tree.java.ParameterElement
+import org.jdom.Document
 import org.jdom.Element
 
 
@@ -12,6 +14,7 @@ class SystemDeclarationEcoConverter(fbmeElement: Element) {
         switchApplicationNetworkDataConnectionsPosition(ecoElement)
         switchResourceNetworkDataConnectionsPosition(ecoElement)
         fixResourceEventConnections(ecoElement)
+        fixInconsistencies(ecoElement)
         return ecoElement
     }
 
@@ -75,6 +78,77 @@ class SystemDeclarationEcoConverter(fbmeElement: Element) {
                     } else if (connectionSourceFix == connectionSourceFix2) {
                         connectionSourceFix = null
                     }
+                }
+            }
+        }
+    }
+
+    private fun fixInconsistencies(ecoElement: Element) {
+        // Changes in applications won't show up in resources.
+        // Exporting an inconsistent file will crash Ecostruxure.
+
+        fun isDeployFile(rootElement: Element): Boolean {
+            // There are two System.sys files in the FBME root directory,
+            // one in bin/Deploy/System/ and another in System/.
+            val applicationElements = rootElement.getChildren("Application")
+            return applicationElements.isEmpty() // Deploy file has no Applications.
+        }
+
+        if (isDeployFile(ecoElement)) return
+
+        val applicationElements = ecoElement.getChildren("Application")
+
+        val deviceElements = ecoElement.getChildren("Device")
+        for (deviceElement in deviceElements) {
+            val resourceElements = deviceElement.getChildren("Resource")
+            for (resourceElement in resourceElements) {
+                val fbNetworkElement = resourceElement.getChild("FBNetwork") ?: continue
+
+                // Check interface constants.
+                val resourceFBElements = fbNetworkElement.getChildren("FB")
+                for (resourceFBElement in resourceFBElements) {
+                    for (applicationElement in applicationElements) {
+                        val subAppNetworkElement = applicationElement.getChild("SubAppNetwork") ?: continue
+                        val applicationFBElements = subAppNetworkElement.getChildren("FB")
+                        for (applicationFBElement in applicationFBElements) {
+                            // Find the matching resourceFBElement, applicationFBElement pair.
+                            // TODO("Not sure if this comparison is 100% reliable:")
+                            val nameA = applicationFBElement.getAttributeValue("Name")
+                            val nameR = resourceFBElement.getAttributeValue("Name")
+                            val typeA = applicationFBElement.getAttributeValue("Type")
+                            val typeR = resourceFBElement.getAttributeValue("Type")
+                            if (nameA != nameR || typeA != typeR) continue
+                            // Matching resourceFBElement, applicationFBElement pair found.
+                            val applicationParameterElements = applicationFBElement.getChildren("Parameter")
+                            if (applicationParameterElements.isEmpty()) continue
+                            val resourceParameterElements = resourceFBElement.getChildren("Parameter")
+                            // The FB element in Application and in Resource should have constant input(s), let's check they match.
+                            // Any FB element could have multiple constant inputs.
+                            for (applicationParameterElement in applicationParameterElements) {
+                                for (resourceParameterElement in resourceParameterElements) {
+                                    val parameterNameA = applicationParameterElement.getAttributeValue("Name")
+                                    val parameterNameR = resourceParameterElement.getAttributeValue("Name")
+                                    if (parameterNameA != parameterNameR) continue
+                                    val valueA = applicationParameterElement.getAttributeValue("Value")
+                                    val valueR = resourceParameterElement.getAttributeValue("Value")
+                                    if (valueA == valueR) continue
+                                    // Found a mismatch, set "resourceParameterElement" correct.
+                                    resourceParameterElement.setAttribute("Value", valueA)
+                                }
+                            }
+                        }
+                    }
+                }
+                continue
+                TODO("Check the connections.")
+                val eventConnectionsElement = fbNetworkElement.getChild("EventConnections") ?: continue
+                for (eventConnectionElement in eventConnectionsElement.children) {
+                    val source = eventConnectionElement.getAttributeValue("Source")
+
+                }
+                val dataConnectionsElement = fbNetworkElement.getChild("DataConnections") ?: continue
+                for (dataConnectionElement in dataConnectionsElement.children) {
+                    val source = dataConnectionElement.getAttributeValue("Source")
                 }
             }
         }
