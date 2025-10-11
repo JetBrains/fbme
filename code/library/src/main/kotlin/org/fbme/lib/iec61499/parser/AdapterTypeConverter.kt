3d165543dabf9716ba815cf8910ea441db2903a3
Esa Paavilainen <esa.paavilainen@aalto.fi>
2025-10-12 00:18:01 +0300

    Moved ServiceDeclarationConverter to a private
    class called AdapterServiceCoverter within the AdapterTypeConverter
    class. It is not needed elsewhere, so this is a fitting spot for it.

--- code/library/src/main/kotlin/org/fbme/lib/iec61499/parser/AdapterTypeConverter.kt
+++ code/library/src/main/kotlin/org/fbme/lib/iec61499/parser/AdapterTypeConverter.kt
@@ -9,7 +9,52 @@ class AdapterTypeConverter(arguments: ConverterArguments) :
         val declaration = factory.createAdapterTypeDeclaration(identifier)
         AuxiliaryDataConverter(this, declaration).extractAuxiliaryData()
         FBInterfaceConverter(this, declaration).extractInterface()
-        ServiceDeclarationConverter(this, declaration).extractService()
+        AdapterServiceConverter(this, declaration).extractService()
         return declaration
+    }
+
+    private class AdapterServiceConverter(arguments: ConverterArguments, private val declaration: AdapterTypeDeclaration) :
+        ConverterBase(arguments) {
+
+        fun extractService() {
+
+            val serviceElement = element?.getChild("Service") ?: return
+            val serviceSequenceElements = serviceElement.getChildren("ServiceSequence")
+            var firstIteration = true
+
+            serviceSequenceElements.forEach { serviceSequenceElement ->
+
+                val serviceTransactionElements = serviceSequenceElement.getChildren("ServiceTransaction")
+
+                serviceTransactionElements.forEach { serviceTransactionElement ->
+
+                    val inputPrimitiveDeclaration = factory.createIOPrimitiveDeclaration()
+                    val inputPrimitiveElement = serviceTransactionElement.getChild("InputPrimitive")
+                    inputPrimitiveDeclaration.iface = inputPrimitiveElement.getAttributeValue("Interface")
+                    inputPrimitiveDeclaration.event = inputPrimitiveElement.getAttributeValue("Event")
+                    inputPrimitiveDeclaration.parameters = inputPrimitiveElement.getAttributeValue("Parameters")
+
+                    val outputPrimitiveDeclaration = factory.createIOPrimitiveDeclaration()
+                    val outputPrimitiveElement = serviceTransactionElement.getChild("OutputPrimitive")
+                    outputPrimitiveDeclaration.iface = outputPrimitiveElement.getAttributeValue("Interface")
+                    outputPrimitiveDeclaration.event = outputPrimitiveElement.getAttributeValue("Event")
+                    outputPrimitiveDeclaration.parameters = outputPrimitiveElement.getAttributeValue("Parameters")
+
+                    val serviceTransaction = factory.createServiceTransactionDeclaration()
+                    serviceTransaction.input = inputPrimitiveDeclaration
+                    serviceTransaction.output = outputPrimitiveDeclaration
+
+                    if (firstIteration) {
+                        declaration.requestServiceSequence.add(serviceTransaction)
+                    } else {
+                        declaration.responseServiceSequence.add(serviceTransaction)
+                    }
+                }
+                firstIteration = false
+            }
+
+            declaration.rightInterface = serviceElement.getAttributeValue("RightInterface")
+            declaration.leftInterface = serviceElement.getAttributeValue("LeftInterface")
+        }
     }
 }
